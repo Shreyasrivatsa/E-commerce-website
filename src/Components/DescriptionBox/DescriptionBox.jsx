1f94b6dd67bd57199a891d1f54d00f33f78032ae
Shreyasrivatsa <shreyasrivatsa25@gmail.com>
2025-05-23 18:43:06 +0530

    DescriptionBox.jsx

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,19 +1,1 @@
-import React from 'react'
-import './DescriptionBox.css'
 
-const DescriptionBox = () => {
-  return (
-    <div className='descriptionbox'>
-        <div className="descriptionbox-nav">
-            {/* <div className="descriptionbox-navbox"> Description</div>
-            <div className="descriptionbox-navbox fade">Reviews(122) </div> */}
-        </div>
-        <div className="descriptionbox-description">
-            <p></p>
-        </div>
-      
-    </div>
-  )
-}
-
-export default DescriptionBox
