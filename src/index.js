1a937302c03794c11e71c1f1919965793eea446e
Shreyasrivatsa <shreyasrivatsa25@gmail.com>
2025-05-23 18:55:07 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import ShopContextProvider from './Context/ShopContext';
-// import { BrowserRouter } from 'react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   // <React.StrictMode>
-//   //   <App />
-//   // </React.StrictMode>
- 
-//       <ShopContextProvider>
-//            <App />
-
-
-//   </ShopContextProvider>
-  
-  
-  
-  
-  
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-//final ->change
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import ShopContextProvider from './Context/ShopContext';
-// import { BrowserRouter } from 'react-router-dom';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter>
-//     <ShopContextProvider>
-//       <App />
-//     </ShopContextProvider>
-//   </BrowserRouter>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -68,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <ShopContextProvider>
-      <DataProductProvider> {/* ✅ Wrap App inside here */}
+      <DataProductProvider> 
         <App />
       </DataProductProvider>
     </ShopContextProvider>
